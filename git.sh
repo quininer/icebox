24fe280d9a638e903d23d07588ee250242ab93a4
quininer <quininer@live.com>
2014-11-03 15:08:01 +0800

    add marked.js
    改了一下，由前端解析MarkDown。
    接下来是要用RSS代替原本的文章列表。

--- git.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd $1
-git add .
-git commit -am "New mark, $2"
-git push box master
--- icebox.py
+++ icebox.py
@@ -7,14 +7,11 @@ from sys import version_info, path as spath
 
 from os import listdir, system, remove, rename
 from subprocess import Popen as popen
-from re import compile as rcompile
 from optparse import OptionParser
-from markdown import markdown
 from json import loads, dumps
 from time import ctime
 
 setting = loads(open((spath[0] or '.')+'/setting.json').read())
-_style = "<link rel='stylesheet' href='{style}' type='text/css'>\n"
 
 def osexec(name, path, ends):
     for ext in setting['extend']:
@@ -24,58 +21,57 @@ def osexec(name, path, ends):
 
 def additem(name, link, path, hide):
     item = loads(open(path+'/item.json').read())
-    if not (name in [item[i][0] for i in item] or hide):
-        item[str(int(item and max(item) or 0)+1)] = [name, '.' + link]
-        open(path+'/item.json', 'w').write(dumps(item))
+    if not (name in item or hide):
+        item.append(name)
+        open('{path}/blog.json'.format(path=path), 'w').write(dumps(item))
 
     osexec(name, path, '')
 
-def markout(name, md_path, path, hide):
-    (styles, strikethrough) = ('', rcompile(r'~~(.+?)~~'))
-    mhtml = strikethrough.sub((lambda x:'<del>'+x.group(1)+'</del>'),
-            markdown(open(md_path+'/'+name+'.md').read().decode('utf8')))
-    for css in setting['style']: styles += _style.format(style=css)
+def markout(name, markpath, path, hide):
+    mark = open('{mark}/{name}.md'.format(mark=markpath, name=name)).read().decode('utf8')
+    open(
+        '{mark}/{name}.md'.format(
+            mark=markpath,
+            name=name
+        ), 'w'
+    ).write(mark.encode('utf8'))
 
-    html = open(setting['tpl'].format(path=path)).read().decode('utf8')
-    html = html.format(title=name, style=styles, mark=mhtml)
-    link = setting['html'].format(path='') + '/' + name + '.html'
-    open(path+link, 'w').write(html.encode('utf8'))
+    additem(name, markpath, hide)
 
-    additem(name, link, path, hide)
-
-def editor(name, md_path, path, hide):
-    command = (setting['editor'], md_path+'/'+name+'.md')
+def editor(name, markpath, path, hide):
+    command = (setting['editor'], '{mark}/{name}.md'.format(mark=markpath, name=name))
     try: popen(command).wait()#FIXME    使用Gvim的话，会不等待，往下执行。
     except Exception: system(' '.join(command))
-    markout(name, md_path, path, hide)
+    markout(name, markpath, path, hide)
 
-def rmvx(name, md_path, path, new, hide):
-    items = loads(open(path+'/item.json').read())
-    if not hide: item = [i for i in items if items[i][0] == name][0]
+def rmvx(name, markpath, path, new, hide):
+    item = loads(open('{path}/blog.json'.format(path=path)).read())
     if not new:
-        if not hide: del items[item]
-        remove(md_path+'/'+name+'.md')
-        remove(setting['html'].format(path=path)+'/'+name+'.html')
+        if not hide: del item[item.index(name)]
+        remove('{mark}/{name}.md'.format(mark=markpath, name=name))
     else:
-        if not hide: (items[item][0], items[item][1]) = (new, './h/'+new+'.html')
-        rename(md_path+'/'+name+'.md', md_path+'/'+new+'.md')
-        rename(setting['html'].format(path=path)+'/'+name+'.html',
-                setting['html'].format(path=path)+'/'+new+'.html')
+        if not hide:
+            item[item.index(name)] = new
+        rename(
+            '{mark}/{name}.md'.format(mark=markpath, name=name),
+            '{mark}/{name}.md'.format(mark=markpath, name=new)
+        )
 
-    if not hide: open(path+'/item.json', 'w').write(dumps(items).encode('utf8'))
+    if not hide: open('{path}/blog.json'.format(path=path), 'w').write(dumps(item).encode('utf8'))
     osexec(name, path, ('重名' if new else '删除'))
 
 def main(name, rmv=False):
     path = setting['path']
-    md_path = setting['md'].format(path=path)
-    if not (name+'.md') in listdir(md_path):
+    markpath = setting['md'].format(path=path)
+    hide = (name[0]=='@')
+    if not '{name}.md'.format(name=name) in listdir(markpath):
         if rmv is False:
             vi = "# {name}\n## *{time}*\n\n".format(name=name, time=ctime())
-            open(md_path+'/'+name+'.md', 'w').write(vi.encode('utf8'))
-    elif rmv != False: rmvx(name, md_path, path, (rmv and rmv.decode('utf8')), (name[0]=='@'))
+            open('{mark}/{name}.md'.format(mark=markpath, name=name), 'w').write(vi.encode('utf8'))
+    elif rmv != False: rmvx(name, markpath, path, (rmv and rmv.decode('utf8')), hide)
     if rmv != False: exit('[x] 未找到该文章.')
 
-    editor(name, md_path, path, (name[0]=='@'))
+    editor(name, markpath, path, hide)
 
 if __name__ == '__main__':
     parser = OptionParser(usage='Usage: %prog [options] Name')
